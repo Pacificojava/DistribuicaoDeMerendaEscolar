f8699a8a13bc1dc7cd4a0dcd8dc59f4b475fd970
helton pacifico <heltonroberto6@gmail.com>
2026-03-24 12:56:13 -0300

    soma e destino da merenda

--- src/DistribuicaoAlimentos/Escolas.java
+++ src/DistribuicaoAlimentos/Escolas.java
@@ -6,19 +6,16 @@ import java.util.Scanner;
 public class Escolas {
 		public String Nomes;
 		public int QAlunos;
+		public double Destinado;
 		
 		//CONSTRUTOR VAZIO
 			public Escolas() {
 		
 		}
 	
-	public static void main(String[] args) {
-		//INICIALIZEI O SCANNER
-        Scanner scanner = new Scanner(System.in);
-        List<Escolas> ListadeEscolas = new ArrayList<>();
-        
+	public void adicionarEscolas(List<Escolas> lista, Scanner scanner) {
         //LAÇO FOR QUE DENTRO DELE CRIEI UM OBJETO DA ESCOLA (escolax), E RECEBENDO TAMBEM O NOME E A QANTIDADE DE ALUNOS 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 2; i++) {
         	Escolas escolax = new Escolas();
         
         	System.out.print("Digite o nome da escola: ");
@@ -27,12 +24,12 @@ public class Escolas {
         	escolax.QAlunos = scanner.nextInt();
 		
         	scanner.nextLine();
-        	ListadeEscolas.add(escolax);
+        	lista.add(escolax);
         	System.out.println("ESCOLA INCLUIDA NA LISTA\n");
         	}
 		
 		System.out.println("______________________LISTA DE ESCOLAS_____________________");
-		for (Escolas e : ListadeEscolas) {
+		for (Escolas e : lista) {
 			
 		System.out.println("ESCOLA: " +  e.Nomes + " | QUANTIDADE DE ALUNOS:  " + e.QAlunos);
 		
--- src/DistribuicaoAlimentos/Main.java
+++ src/DistribuicaoAlimentos/Main.java
@@ -15,7 +15,31 @@ public class Main {
 		List<Alimentos> listadeAlimentos = new ArrayList<>();
 
 		alimento.adicionarAlimentos(listadeAlimentos, scanner);
-			
+		
+		Escolas escola = new Escolas();
+		List<Escolas> listadeEscolas = new ArrayList<>();
+		
+		escola.adicionarEscolas(listadeEscolas, scanner);
+		
+		// Calcular totais
+		int TotalAlimento = 0;
+		for (Alimentos a : listadeAlimentos) {
+		    TotalAlimento += a.qGramas;
+		}
+		
+		int TotalAlunos = 0;
+		for (Escolas e : listadeEscolas) {
+		    TotalAlunos += e.QAlunos;
+		}
+		
+		// Calcular Destinado para cada escola
+		System.out.println("\n______________________DISTRIBUIÇÃO DE ALIMENTOS_____________________");
+		for (Escolas e : listadeEscolas) {
+		    e.Destinado = (double)(TotalAlimento * e.QAlunos) / TotalAlunos;
+		    System.out.println("ESCOLA: " + e.Nomes + " | DESTINADO: " + e.Destinado + " gramas");
+		}
+		
+		scanner.close();
 	}
 
 
